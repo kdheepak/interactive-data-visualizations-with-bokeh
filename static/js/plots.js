bff6a4ddfc0b8b34d75865632529ad9f6fc4b8e1
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2017-03-22 01:48:46 -0600

    Update plots.js

--- static/js/plots.js
+++ static/js/plots.js
@@ -365,7 +365,7 @@
             }
             store[msg.from]['languages'] = l.filter( onlyUnique )
         }
-        if (body.toLowerCase().includes('libraries:')) {
+        elif (body.toLowerCase().includes('libraries:')) {
             var l = body.split(":")[1].trim().replace(/\,$/, '').split(',').filter( onlyUnique ).filter(String)
             for(var i=0; (i<l.length && i<10); i++){
                 if(l[i].length>10) {
